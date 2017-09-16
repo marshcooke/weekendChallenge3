537dc3744ae2d66821237ab55905c6c77665e1d3
Sarah Cooke <marshcooke1@gmail.com>
2017-09-16 00:22:56 -0500

    spinning up server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var path = require('path');
 
 app.use(express.static('public'));
 
 app.listen(port, function(){
     console.log('listening on port', port);
 })
+
+app.get('/', function(req, res){
+    console.log('sending / to DOM');
+    var indexPath = path.join(__dirname, '/./public/views/index.html');
+    res.sendFile(indexPath);
+});
